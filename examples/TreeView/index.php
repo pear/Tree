c80cdf09d97662a7a93b6833599a6a58887900fa
Wolfram Kriesing <cain@php.net>
2002-05-04 19:21:20 +0000

    - tree view example, for nested trees
    git-svn-id: http://svn.php.net/repository/pear/packages/Tree/trunk@81229 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/TreeView/index.php
@@ -0,0 +1,81 @@
+<?php
+
+    //
+    //  $Id$
+    //
+
+    ##################################################
+    #
+    #       init template engine
+    #
+    // you need the template class from http://sf.net/projects/simpltpl
+    if( !@include('SimpleTemplate/Engine.php') )
+    {
+        print   'sorry, you need the template class from <a href="http://sf.net/projects/simpltpl">sourceforge</a> for this.<br>'.
+                'or if i have time i put the examples <a href="http://wolfram.kriesing.de/programming/">here online</a>';
+        die();
+    }
+    require_once('SimpleTemplate/Filter/TagLib.php');
+    $options = array(   'templateDir'   => $_SERVER['DOCUMENT_ROOT'].'pear/Tree/examples/TreeView' ,
+                        'compileDir'    => $_SERVER['DOCUMENT_ROOT'].'pear/Tree/examples/TreeView/tmp',
+                        'forceCompile'  => true );
+    $tpl = new SimpleTemplate_Engine($options);
+    $tagLib = new SimpleTemplate_Filter_TagLib($tpl->options);
+    $tpl->registerPrefilter(array(&$tagLib,'repeat'));
+    $tpl->registerPrefilter(array(&$tagLib,'block'));
+    $tplFilter = new SimpleTemplate_Filter_Basic($tpl->options);
+    $tpl->registerPrefilter(array(&$tplFilter,'addIfBeforeForeach'));
+
+
+    ##################################################
+    #
+    #       actual tree stuff, using Dynamic_DBnested
+    #
+    require_once('Tree/Tree.php');
+    $tree = Tree::setup( 'Dynamic_DBnested' , 'mysql://root@localhost/test' , array('table'=>'nestedTree') );
+
+    if( $_REQUEST['action_add'] )
+    {
+        $methodCall = "tree->add( {$_REQUEST['newData']} , {$_REQUEST['parentId']} , {$_REQUEST['prevId']} )";
+        $result = $tree->add( $_REQUEST['newData'] , $_REQUEST['parentId'] , $_REQUEST['prevId'] );
+    }
+
+    if( $_REQUEST['action_remove'] )
+    {
+        $methodCall = "$tree->remove( {$_REQUEST['removeId']} )";
+        $result = $tree->remove( $_REQUEST['removeId'] );
+    }
+
+    if( $_REQUEST['action_update'] )
+    {
+        $methodCall = "tree->update( {$_REQUEST['updateId']} , {$_REQUEST['updateData']} )";
+        $result = $tree->update( $_REQUEST['updateId'] , $_REQUEST['updateData'] );
+    }
+
+    if( $_REQUEST['action_move'] )
+    {
+        $methodCall = "tree->move( {$_REQUEST['move_id']} , {$_REQUEST['move_newParentId']} , {$_REQUEST['move_newPrevId']} )";
+        $result = $tree->move( $_REQUEST['move_id'] , $_REQUEST['move_newParentId'] , $_REQUEST['move_newPrevId'] );
+    }
+
+
+
+    if( !$fid )
+        $fid = $tree->getRootId();
+
+    $path = $tree->getPath( $fid );
+    $children = $tree->getChildren( $fid );
+
+    ##################################################
+    #
+    #       actual tree stuff to show the entire tree using Memory_DBnested
+    #
+    require_once('Tree/Tree.php');
+    $memTree = Tree::setup( 'Memory_DBnested' , 'mysql://root@localhost/test' , array('table'=>'nestedTree') );
+
+    $memTree->setup();
+    $entireTree = $memTree->getNode();
+
+    $tpl->compile('index.tpl');
+    include($tpl->compiledTemplate);
+?>
