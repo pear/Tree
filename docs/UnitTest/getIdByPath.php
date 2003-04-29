736b617b2f4459c158de709660b7e7eb0f90a8eb
Wolfram Kriesing <cain@php.net>
2003-04-29 11:01:22 +0000

    - initial commit
    git-svn-id: http://svn.php.net/repository/pear/packages/Tree/trunk@125073 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/UnitTest/getIdByPath.php
@@ -0,0 +1,38 @@
+<?php
+//
+//  $Id$
+//
+
+require_once 'UnitTest.php';
+
+class UnitTest_getIdByPath extends UnitTest
+{
+    // check if we get the right ID, for the given path
+    function test_MemoryDBnested()
+    {
+        $tree = $this->getMemoryDBnested();        
+        $id = $tree->getIdByPath('/Root/child 2/child 2_2');
+        
+        $this->assertEquals(5,$id);
+    }
+
+    // do this for XML
+            
+    // do this for Filesystem
+
+    // do this for DBsimple
+    
+    // do this for DynamicDBnested
+    function test_DynamicDBnested()
+    {
+        $tree = $this->getDynamicDBnested();
+        $id = $tree->getIdByPath('/Root/child 2/child 2_2');
+        
+        $this->assertEquals(5,$id);
+    }
+    
+
+
+}
+
+?>
