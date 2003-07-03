a0c3f3989913ae7c343867c9bb429ad9b2151f2d
Lorenzo Alberton <quipo@php.net>
2003-07-03 18:20:50 +0000

    remove E_ALL notice
    git-svn-id: http://svn.php.net/repository/pear/packages/Tree/trunk@134128 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/index.php
+++ tests/index.php
@@ -3,13 +3,12 @@
 //  $Id$
 //
 
-
 ini_set('include_path',realpath(dirname(__FILE__).'/../../../').':'.realpath(dirname(__FILE__).'/../../../../includes').':'.ini_get('include_path'));
 require_once 'PHPUnit.php';
 require_once 'PHPUnit/GUI/HTML.php';
 
-define(DB_DSN,'mysql://root@localhost/test');
-define(TABLE_TREENESTED,    'TreeNested');
+define('DB_DSN',           'mysql://root@localhost/test');
+define('TABLE_TREENESTED', 'TreeNested');
 
 require_once 'Tree/Tree.php';
 
